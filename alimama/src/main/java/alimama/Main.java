3b2dd9210875923ea9c7cfd6b2763abbb27bfed9
zhangzengmin <zhangzengmin@cydeng.com>
2016-09-20 11:36:39 +0800

    update

--- alimama/src/main/java/alimama/Main.java
+++ alimama/src/main/java/alimama/Main.java
@@ -56,7 +56,8 @@ public class Main {
 		
 		String localWebDriver=PropertiesUtil.getPropertiesMap("localWebDriver");
 		if("true".equals(localWebDriver)){
-			webDriver = new FirefoxDriver();
+			//webDriver = new FirefoxDriver();
+			webDriver = SeleniumUtil.initChromeDriver();
 		}else{
 			webDriver =SeleniumUtil.initWebDriver("http://192.168.1.120:4444/wd/hub");
 		}
