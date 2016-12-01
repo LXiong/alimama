1662ce3962559bdc058257e76b9cd073453d28eb
zhangzengmin <zhangzengmin@cydeng.com>
2016-12-01 16:15:27 +0800

    update

---
+++ alimama/src/main/java/fx/IPDao.java
@@ -0,0 +1,50 @@
+package fx;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.io.FileUtils;
+
+
+public class IPDao {
+	
+	public File dbFile = null;
+	Map<String,String> ipMap = new ConcurrentHashMap<String, String>();
+	
+	public IPDao(File dbFile){
+		this.dbFile = dbFile;
+		try {
+			if(!dbFile.exists()){
+				return ;
+			}
+			List<String> list = FileUtils.readLines(dbFile);
+			for(String ip:list){
+				ipMap.put(ip, ip);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	public void addIp(String ip){
+		synchronized(ipMap.getClass()){
+			ipMap.put(ip, ip);
+			try {
+				FileUtils.write(dbFile, ip+"\r\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public boolean containsKey(String ip){
+		return ipMap.containsKey(ip);
+	}
+
+}
--- alimama/src/main/java/fx/Main.java
+++ alimama/src/main/java/fx/Main.java
@@ -1,15 +1,48 @@
 package fx;
 
+import java.io.File;
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.http.HttpHost;
 
 public class Main {
 	
-	public void execute(String ip,int prot,String url)throws Exception{
-		HttpHost host = new HttpHost(ip, prot);;
-		HttpTest http = new HttpTest();
-		http.aurl = url;
-		http.host = host;
-		http.execute();
+	String fileName = "c:\\rrz"+DateFormatUtils.format(new Date(), "yyyyMMdd");
+	
+	IPDao ipDao = new IPDao(new File(fileName));
+	
+	public void execute(String ip,int prot,String url){
+		try{
+			HttpHost host = new HttpHost(ip, prot);;
+			HttpTest http = new HttpTest();
+			http.aurl = url;
+			http.host = host;
+			http.execute();
+			System.out.println("execute ok>>>>>>>>>>>>>>>>>>>>>>");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void executeAll()throws Exception{
+		HttpHost httpHost = getIp();
+		System.out.println("httpHost :"+httpHost);
+		boolean flag = checkIp(httpHost.getHostName(), httpHost.getPort());
+		System.out.println("checkIp : "+flag);
+		if(flag){
+			String url = getUrl();
+			System.out.println("aurl : " +url);
+			execute(httpHost.getHostName(), httpHost.getPort(), url);
+			destroy(httpHost.getHostName());
+		}
+		
+	}
+	
+     public String getUrl(){
+		
+		return null;
 	}
 	
 	public HttpHost getIp(){
@@ -18,12 +51,17 @@ public class Main {
 	}
 	
 	public boolean checkIp(String ip,int prot){
-	
+	    boolean flag = ipDao.containsKey(ip);
+	    if(!flag){
+	    	//checkIp
+	    	
+	    	return true;
+	    }
 		return false;
 	}
 	
 	public void destroy(String ip){
-		
+		ipDao.addIp(ip);
 	}
 
 }
