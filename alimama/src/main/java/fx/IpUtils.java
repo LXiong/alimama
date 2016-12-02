b1baec1c8317d06defb2b523f2ac54c7e1ff8e35
zhangzengmin <zhangzengmin@cydeng.com>
2016-12-02 20:56:41 +0800

    update

--- alimama/src/main/java/fx/IpUtils.java
+++ alimama/src/main/java/fx/IpUtils.java
@@ -16,7 +16,8 @@ public class IpUtils {
 	// http://ip.memories1999.com/api.php?dh=2764810913906166&sl=2&xl=%E5%9B%BD%E5%86%85&gl=1
 	public static List<HttpHost> getIpsmemories1999() {
 		//return getips("http://ip.memories1999.com/api.php?dh=2764810913906166&sl="+Execute.poolSize+"&xl=%E5%9B%BD%E5%86%85&gl=1");
-		return getips("http://ip.memories1999.com/api.php?dh=2764810913906166&sl="+Execute.poolSize+"&xl=%E5%9B%BD%E5%86%85");
+		String url = PropertiesUtil.getProperty("ipurlrrz");
+		return getips(url);
 	}
 
 	public  static List<HttpHost> getips(String url) {
