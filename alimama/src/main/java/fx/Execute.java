f10b89a5e1fd535c3b0466d723863ebdef55963c
zhangzengmin <zhangzengmin@cydeng.com>
2016-12-02 19:28:34 +0800

    update

--- alimama/src/main/java/fx/Execute.java
+++ alimama/src/main/java/fx/Execute.java
@@ -85,6 +85,6 @@ public class Execute {
 		ScheduledExecutorService service = Executors
 				.newSingleThreadScheduledExecutor();
 		// 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
-		service.scheduleAtFixedRate(runnable, 2, 10, TimeUnit.SECONDS);
+		service.scheduleAtFixedRate(runnable, 2, 3, TimeUnit.SECONDS);
 	}
 }
