ed6ee4b161b3427e5cc8f8291c54134c215c52da
xiaomin0322 <xiaomin0322@sian.com>
2017-04-20 21:15:24 +0800

    12323

--- alimama/src/main/java/dataoke/Ma60.java
+++ alimama/src/main/java/dataoke/Ma60.java
@@ -105,7 +105,7 @@ public class Ma60 {
 				+ userKey
 				+ "&dockcode=10692408025D0DB&telnum="
 				+ telnum;
-		HttpRequest httpRequest = HttpRequest.get(url);
+		HttpRequest httpRequest = HttpRequest.get(url).timeout(5000);
 		HttpResponse response = httpRequest.send();
 		// response.accept("utf-8");
 		// response.acceptEncoding("utf-8");
