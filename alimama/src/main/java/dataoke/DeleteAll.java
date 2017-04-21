27bae0bb3e455962e4dbcfe209c5825b497d8300
xiaomin0322 <xiaomin0322@sian.com>
2017-04-21 22:13:38 +0800

    123123

--- alimama/src/main/java/dataoke/DeleteAll.java
+++ alimama/src/main/java/dataoke/DeleteAll.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class DeleteAll {
 	
 	public static void main(String[] args) throws Exception{
-		File[] files = new File[]{new File("G:\\taoke\\第1组500.txt"),new File("G:\\taoke\\第2组500.txt")
-		,new File("G:\\taoke\\第3组500.txt"),new File("G:\\taoke\\第4组.txt")};
+		File[] files = new File[]{new File("d:\\taoke\\第1组500.txt"),new File("d:\\taoke\\第2组500.txt")
+		,new File("d:\\taoke\\第3组500.txt"),new File("d:\\taoke\\第4组.txt")};
 		Test.execteDeleteAll(files);
 	}
 
--- alimama/src/main/java/dataoke/Refresh.java
+++ alimama/src/main/java/dataoke/Refresh.java
@@ -16,8 +16,8 @@ public class Refresh {
 	
 		//executeTest();
 	  // testck();
-		File[] files = new File[]{new File("G:\\taoke\\第1组500.txt"),new File("G:\\taoke\\第2组500.txt")
-		,new File("G:\\taoke\\第3组500.txt"),new File("G:\\taoke\\第4组.txt")};
+		File[] files = new File[]{new File("d:\\taoke\\第1组500.txt"),new File("d:\\taoke\\第2组500.txt")
+		,new File("d:\\taoke\\第3组500.txt"),new File("d:\\taoke\\第4组.txt")};
 		//execteAll("2255030",new File("G:\\taoke\\第4组.txt"));
 		Test.execteAll("2263225",files);
 		
