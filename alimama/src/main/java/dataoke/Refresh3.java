79f57b90663f1767a034f24546037f0a159d3a07
xiaomin0322 <xiaomin0322@sian.com>
2017-04-22 08:07:36 +0800

    123123

--- alimama/src/main/java/dataoke/Refresh3.java
+++ alimama/src/main/java/dataoke/Refresh3.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import jodd.http.Cookie;
 
-public class Refresh {
+public class Refresh3 {
 	
 	static Map<String,Cookie[]> map = new HashMap<String, Cookie[]>();
 	
@@ -16,8 +16,8 @@ public class Refresh {
 	
 		//executeTest();
 	  // testck();
-		File[] files = new File[]{new File("d:\\taoke\\第5组500.txt"),new File("d:\\taoke\\第6组500.txt")
-		,new File("d:\\taoke\\第7组500.txt"),new File("d:\\taoke\\第8组.txt")};
+		File[] files = new File[]{new File("d:\\taoke\\第1组500.txt"),new File("d:\\taoke\\第2组500.txt")
+		,new File("d:\\taoke\\第3组500.txt"),new File("d:\\taoke\\第4组.txt")};
 		//execteAll("2255030",new File("G:\\taoke\\第4组.txt"));
 		Test.execteAll("2263225",files);
 		
