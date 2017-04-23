2b4cc413c30e1059b8d27ab730f126a6f45f6d11
xiaomin0322 <xiaomin0322@sian.com>
2017-04-23 22:05:03 +0800

    123123

--- alimama/src/main/java/dataoke/DeleteAll.java
+++ alimama/src/main/java/dataoke/DeleteAll.java
@@ -6,8 +6,8 @@ public class DeleteAll {
 	
 	public static void main(String[] args) throws Exception{
 		Thread.sleep(1000 * 60 * 60);
-		File[] files = new File[]{new File("d:\\taoke\\大淘客帐号\\B批第1组500.txt"),new File("d:\\taoke\\大淘客帐号\\B批第2组500.txt")
-		,new File("d:\\taoke\\大淘客帐号\\B批第3组500.txt"),new File("d:\\taoke\\大淘客帐号\\B批第4组410.txt")};
+		File[] files = new File[]{new File("d:\\dataoke\\大淘客帐号\\B批第1组500.txt"),new File("d:\\dataoke\\大淘客帐号\\B批第2组500.txt")
+		,new File("d:\\dataoke\\大淘客帐号\\B批第3组500.txt"),new File("d:\\dataoke\\大淘客帐号\\B批第4组410.txt")};
 		Test.execteDeleteAll(files);
 	}
 
