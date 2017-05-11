bf19ea8262ea298c16e4a8e6102ec25871d8dd32
xiaomin0322 <xiaomin0322@sian.com>
2017-05-11 08:24:20 +0800

    123

--- alimama/src/main/java/dataoke/Cmd.java
+++ alimama/src/main/java/dataoke/Cmd.java
@@ -18,9 +18,9 @@ public class Cmd {
 		}
 	}
 	
-	public static int min = 1000;
+	public static int min = 10000;
 	
-	public static int max = 5000;
+	public static int max = 20000;
 	
 	public static int getSleepTime(){
 	   return getSleepTime(min, max);
