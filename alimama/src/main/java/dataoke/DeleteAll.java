689d4559c19b1be0ec3abcf8cf140ae77d5c02a0
zhangzengmin <zhangzengmin@cydeng.com>
2017-05-17 10:26:30 +0800

    123123

--- alimama/src/main/java/dataoke/DeleteAll.java
+++ alimama/src/main/java/dataoke/DeleteAll.java
@@ -5,9 +5,8 @@ import java.io.File;
 public class DeleteAll {
 	
 	public static void main(String[] args) throws Exception{
-		Thread.sleep(1000 * 60 * 60);
-		File[] files = new File[]{new File("d:\\dataoke\\大淘客帐号\\B批第1组500.txt"),new File("d:\\dataoke\\大淘客帐号\\B批第2组500.txt")
-		,new File("d:\\dataoke\\大淘客帐号\\B批第3组500.txt"),new File("d:\\dataoke\\大淘客帐号\\B批第4组410.txt")};
+		//Thread.sleep(1000 * 60 * 60);
+		File[] files = new File[]{new File("d:\\dataoke\\大淘客帐号\\dataokeuse20170510.txt")};
 		Test.execteDeleteAll(files);
 	}
 
