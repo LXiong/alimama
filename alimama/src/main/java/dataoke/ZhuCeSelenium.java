a26b2fef9afe9b4151b5e0b7cacbecb25a24b22d
zhangzengmin <zhangzengmin@cydeng.com>
2017-05-27 15:22:08 +0800

    12313

--- alimama/src/main/java/dataoke/ZhuCeSelenium.java
+++ alimama/src/main/java/dataoke/ZhuCeSelenium.java
@@ -9,6 +9,8 @@ import java.util.Map.Entry;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import jodd.http.Cookie;
@@ -46,6 +48,43 @@ public class ZhuCeSelenium {
 			pids = args[0];
 		}
 
+		
+		 Runnable runnable = new Runnable() {  
+	            public void run() {  
+	                // task to run goes here  
+	            	try{
+	            		 System.out.println("开始检测ip池ip数据数量>>>>>>>>>>>>>>>>>>");
+	 	                int size = blockingQueue.size();
+	 	                System.out.println("当前ip池数量wei:  "+size);
+	 	                if(size<5){
+	 	                	System.out.println("开始获取代理ip>>>>>>>>>>>>>>>>>>>>>>>>>");
+	 	        			//proxyURLList.get(new Random().nextInt(3))
+	 	        			List<HttpHost> hosts = IpUtils.getips(proxyURL);
+	 	        			 if(CollectionUtils.isEmpty(hosts)){
+	 	        				 System.out.println("获取ip为kong>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	 	        			 }else{
+	 	        				 for(HttpHost h:hosts){
+	 	        					 boolean flag = IpUtils.createIPAddress(host.getHostName(),host.getPort());
+	 	        					 if(flag){
+	 	        						 blockingQueue.add(h);
+	 	        					 }
+	 	        					
+	 	        				 }
+	 	        				// System.out.println("获取代理ip成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ip="+hosts.get(0).getHostName()+" prot:"+hosts.get(0).getPort());
+	 	        			 }
+	 	                }else{
+	 	                	System.out.println("当前ip池数据量充足>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	 	                }
+	            	}catch(Exception e){
+	            		e.printStackTrace();
+	            	}
+	               
+	            }  
+	        };  
+	        ScheduledExecutorService service = Executors  
+	                .newSingleThreadScheduledExecutor();  
+	        // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间  
+	        service.scheduleAtFixedRate(runnable, 10, 1, TimeUnit.SECONDS);
 		
 		browser = new HttpBrowser();
 		
@@ -92,7 +131,7 @@ public class ZhuCeSelenium {
 	static HttpHost host = null;
 	
 	//static String proxyURL="http://ip.memories1999.com/api.php?dh=2764810913906166&sl=1&xl=%E5%9B%BD%E5%86%85&gl=1";
-	static String proxyURL="http://www.xsdaili.com/get?orderid=104948606338185&num=7&an_ha=1&an_an=1&sp1=1&sp2=1&dedup=1&gj=1";
+	static String proxyURL="http://www.xsdaili.com/get?orderid=104948606338185&num=10&an_ha=1&an_an=1&sp1=1&sp2=1&dedup=1&gj=1";
 	
 	static List<String> proxyURLList = new ArrayList<String>();
 	
@@ -119,7 +158,11 @@ public class ZhuCeSelenium {
 				 Thread.sleep(3000);
 			 }else{
 				 for(HttpHost h:hosts){
-					 blockingQueue.add(h);
+					 boolean flag = IpUtils.createIPAddress(host.getHostName(),host.getPort());
+					 if(flag){
+						 blockingQueue.add(h);
+					 }
+					
 				 }
 				// System.out.println("获取代理ip成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ip="+hosts.get(0).getHostName()+" prot:"+hosts.get(0).getPort());
 			 }
@@ -336,7 +379,7 @@ public class ZhuCeSelenium {
 			 js.executeScript("document.querySelectorAll(\"a[class='submit-btn register-btn']\")[0].click();");
 			 System.out.println("注册成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 			 
-			 okSize +=1;
+			 okSize +=5;
 			 FileUtils.write(out, num+"----"+pwd+"\r\n",true);
 			 System.out.println("当前ip已经注册成功 >>>>>>>>>>>"+okSize+" 个号！！！！！！！！！！");
 			 Thread.sleep(1000);
