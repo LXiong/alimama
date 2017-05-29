d52318445b2283d969a8b947d91289e2ad24f0fd
xiaomin0322 <xiaomin0322@sian.com>
2017-05-29 10:29:57 +0800

    123123

--- alimama/src/main/java/dataoke/Test.java
+++ alimama/src/main/java/dataoke/Test.java
@@ -5,41 +5,43 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import fx.HttpTest;
 import jodd.http.Cookie;
 import jodd.http.HttpRequest;
 import jodd.http.HttpResponse;
 import jodd.http.ProxyInfo;
 import jodd.http.ProxyInfo.ProxyType;
 import jodd.http.net.SocketHttpConnectionProvider;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpHost;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import util.CharUtil;
 import util.HtmlUnitUtil;
 import util.IpPoolUtil;
 import util.IpUtils;
 import util.MyConnectionProvider;
-
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
-import fx.HttpTest;
 
 public class Test {
 	
@@ -272,6 +274,7 @@ public class Test {
 	public static void setCookis(String uname,HttpRequestBase httpRequest)throws Exception{
 		Map<String, String> cookisMap = new HashMap<String, String>();
 		String cookisStr = "random=8696; ASPSESSIONIDSQCRRSDT=PMFGMKPANNDIPLFIEAGFOJHD; dtk_web=mgbpf1uvaohssnvee7m02u1lt7; UM_distinctid=15b906fc3d99a-0ca45cd09b0c9d-12616a4a-1fa400-15b906fc3db105; CNZZDATA1257179126=1538129784-1492772062-http%253A%252F%252Fwww.dataoke.com%252F%7C1492777462; userid=537000; user_email=15201733860; user%5Femail=15201733860; upe=537e2926; e88a8013345a8f05461081898691958c=834b4337570611838d9b6989521575fb85ae30b6a%3A4%3A%7Bi%3A0%3Bs%3A6%3A%22537000%22%3Bi%3A1%3Bs%3A11%3A%2215201733860%22%3Bi%3A2%3Bi%3A2592000%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D; ASPSESSIONIDSSBQSTCT=ICEPOLPACLKKGLDMHNNFFFIA; ASPSESSIONIDQSCRRTDS=CACEBLPAJEAMCMJMGPHFAEOB; ASPSESSIONIDSQCTQTCS=HNCMFMPAEKHOCBIEFGDHDDLH; ASPSESSIONIDQQCTRTCS=OMKMBNPAOFLEBJBEGOKDNIIF; ASPSESSIONIDQSBTQSCS=OHDGKNPACFIHDDFNANILEPKF; token=8f5d2c916cf9a2051dea789e96780d5d; ASPSESSIONIDSQBQSSDT=KNFMMBABCBPCEFDLDGGAGLJO; ASPSESSIONIDQSASQTDT=CDAFIBABKOLMLCOGGMEINGBM";
+		cookisStr ="UM_distinctid=15b906fc3d99a-0ca45cd09b0c9d-12616a4a-1fa400-15b906fc3db105; CNZZDATA1257179126=1538129784-1492772062-http%253A%252F%252Fwww.dataoke.com%252F%7C1495022035; upe=20d3785a; userid=577118; user_email=13751442412; user%5Femail=13751442412; e88a8013345a8f05461081898691958c=7d9adb0eb6afa7a4eecc7c18902e6085c5ff7f4aa%3A4%3A%7Bi%3A0%3Bs%3A6%3A%22577118%22%3Bi%3A1%3Bs%3A11%3A%2213751442412%22%3Bi%3A2%3Bi%3A2592000%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D; ASPSESSIONIDCSDRDTDQ=GMMCEACCDGPMDPPGONLJOKOE; dtk_web=mbc6880vo5it1sf371aqbiasa5; ASPSESSIONIDSSBQRSDS=JDDNOPBCONNOEFIGOLFPKLPI; token=6d2ae79ff668877fd32526c96d116e4b; random=1366";
 		for (String str : cookisStr.split("\\;")) {
 			cookisMap.put(str.split("\\=")[0].trim(), str.split("\\=")[1]);
 		}
@@ -1007,8 +1010,8 @@ public class Test {
 			    	readExecute(pid, uname);
 					Thread.sleep(500);
 					
-					boolean flagt = tuijian(pid,uname,true);
-					//boolean flagt = tuijianHttpClient(pid,uname);
+					//boolean flagt = tuijian(pid,uname,true);
+					boolean flagt = tuijianHttpClient(pid,uname);
 					//flag = tuijianToFile(pid,uname);
 					//boolean flagt = zhuan(uname,pid);
 					//boolean flagt = TestSelenium.execute(pid,uname);
@@ -1181,11 +1184,18 @@ public class Test {
 		 httpRequest.setHeader("Upgrade-Insecure-Requests", "1");
 		 httpRequest.setHeader("Connection", "keep-alive");
 		 httpRequest.setHeader("X-Requested-With", "XMLHttpRequest");
+		 httpRequest.setHeader("User-Agent", HttpTest.getUserAgent());
+		 
 		 setCookis(uname, httpRequest);
 
-		 HttpClientUtils httpClientUtils = new HttpClientUtils();
+		   HttpClientUtils httpClientUtils = new HttpClientUtils();
 		 
-		 HttpHost proxy = getProxy();
+		    List<NameValuePair> nvps = new ArrayList<NameValuePair>();  
+	        nvps.add(new BasicNameValuePair("act", "add_quan"));  
+	        nvps.add(new BasicNameValuePair("id", id));  
+	        httpRequest.setEntity(new UrlEncodedFormEntity(nvps));  
+	        
+		 HttpHost proxy = IpPoolUtil.getHttpHost();
 		 String rc =  httpClientUtils.getContentByUrl(proxy, httpRequest, 10000);
 		 
 		 if(rc.equalsIgnoreCase("ok")){
