2e020c55bbdee6f02dd188811550dd908f574ddb
xiaomin0322 <xiaomin0322@sian.com>
2017-06-17 17:16:13 +0800

    1231

--- alimama/src/main/java/util/img/ImageHandleHelper.java
+++ alimama/src/main/java/util/img/ImageHandleHelper.java
@@ -245,7 +245,7 @@ public class ImageHandleHelper {
 		   System.out.println("开始生成左边商品描述图片>>>>>>>>>>>>>>>>>>>>>");
 		     //左边图片
 		    File file1 = new File("d:\\dataoke", "1.jpg");  
-		    WordToPic.TextToPicTaoKe(page.getTbGoodsTitle(), /*"原价:￥"+*/page.getTaoBaopOldrice(), /*"现价:￥"+*/page.getTaoBaoprice(), "[￥"+page.getQuanPrice()+"券]", 320, 130,file1);
+		    WordToPic.TextToPicTaoKe(page.getTbGoodsTitle(), "￥"+page.getTaoBaopOldrice(), "￥"+page.getTaoBaoprice(), "[￥"+page.getQuanPrice()+"券]", 320, 130,file1);
 		   
 	        
 		    System.out.println("开始生成推荐二位吗图片>>>>>>>>>>>>>>>>>>>>>");
