d3df742f60aa829f64c16080acb3e55ccffe15e3
zhangzengmin <zhangzengmin@cydeng.com>
2017-07-28 10:37:20 +0800

    123

---
+++ alimama/src/main/java/dataoke/GetGoodIdsByDataoke.java
@@ -0,0 +1,41 @@
+package dataoke;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.jsoup.nodes.Element;
+
+public class GetGoodIdsByDataoke {
+
+	public static void main(String[] args) throws Exception {
+		execute();
+	}
+	
+	static File out = new File("d:\\goodsurls.txt");
+	
+	public static void execute()throws Exception{
+		for(int j=1;j<554;j++){
+			try{
+			List<Element> list = Monitor
+					.getTopIds("http://www.dataoke.com/qlist/?page=2");
+			for (int i = 0; i < list.size(); i++) {
+				Element e = list.get(i);
+				System.out.println(e);
+				//String pid = e.attr("id").replace("goods-items_", "");
+				//System.out.println("pid==="+pid);
+				String goodsId = e.attr("data_goodsid");
+				String url = "https://detail.tmall.com/item.htm?id="+goodsId;
+				FileUtils.write(out, url+"\r\n",true);
+				System.out.println(url);
+			}
+			
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			Thread.sleep(1000);
+		}
+		
+	}
+
+}
