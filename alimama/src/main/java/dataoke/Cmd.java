d20c7c8838a51d8f84b27eb6b954e66fe52187c9
zhangzengmin <zhangzengmin@cydeng.com>
2017-08-23 13:52:54 +0800

    12313

--- alimama/src/main/java/dataoke/Cmd.java
+++ alimama/src/main/java/dataoke/Cmd.java
@@ -14,6 +14,8 @@ import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
+import util.IpMocUtils;
+
 public class Cmd {
 	
      static Map<String,File> map = new HashMap<String, File>();
@@ -39,7 +41,16 @@ public class Cmd {
         return s;
 	}
 	
+	
+	final static String mac = "1C-1B-0D-78-F0-93";
 	public static void main(String[] args) throws Exception{
+		
+		if(!mac.equalsIgnoreCase(IpMocUtils.getMACAddress())){
+			  Test.printLog("mac校验失败>>>>>>>>>>>>>>>>>>>>>>>>>>清联系管理员");
+				return ;
+		  }else{
+			  Test.printLog("mac 校验成功>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		  }
 		
 		//Thread.sleep(1000 * 60 * 60);
 		//args = new String[]{"2882511,2895769","103","5000,20000","1"}; 
