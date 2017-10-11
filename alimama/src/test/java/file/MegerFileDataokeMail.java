bc7bda6a2e07973907eb494106220e0338276a4e
zhangzengmin <zhangzengmin@cydeng.com>
2017-10-11 18:52:05 +0800

    123123

--- alimama/src/test/java/file/MegerFileDataokeMail.java
+++ alimama/src/test/java/file/MegerFileDataokeMail.java
@@ -10,7 +10,29 @@ public class MegerFileDataokeMail {
 	static File base = new File("D:\\dataoke\\邮箱账号\\20170902");
 	
 	public static void main(String[] args)throws Exception {
-		File dataoke = new File(base,"dataoke.txt");
+		megerDataokeAndTaobao();
+	}
+	
+	
+	public static void megerDataokeAndTaobao()throws Exception{
+        File dataoke = new File(base,"dataoke.txt");
+		
+		File mail = new File(base,"taobao.txt");
+		
+		List<String> mails = FileUtils.readLines(mail);
+		
+		int i = 0;
+		for(String m:FileUtils.readLines(dataoke)){
+			 System.out.println("index==="+i);
+			 
+		    FileUtils.write(new File(base,"all.txt"), m+"----"+mails.get(i).replace(":", "----")+"\r\n",true);
+           i +=1;
+          
+		}
+	}
+	
+	public static void megerDataokeAndMail()throws Exception{
+         File dataoke = new File(base,"dataoke.txt");
 		
 		File mail = new File(base,"mail.txt");
 		
