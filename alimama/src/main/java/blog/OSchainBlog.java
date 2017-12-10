ff19b852d38dae7f67654bc9bd9a1fffd8c74f27
xiaomin0322 <xiaomin0322@sina.com>
2017-12-10 18:00:04 +0800

    blog

--- alimama/src/main/java/blog/OSchainBlog.java
+++ alimama/src/main/java/blog/OSchainBlog.java
@@ -1,17 +1,18 @@
 package blog;
 
 import java.io.File;
-import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -21,7 +22,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import util.HttpClientUtil;
-import util.SeleniumUtil;
 
 public class OSchainBlog {
 	
@@ -31,23 +31,142 @@ public class OSchainBlog {
 	
 	public static void main(String[] args)throws Exception {
 		
-	          send("vvvvvvvvvv", "%3Cp%3Eweqrqerqer%3C%2Fp%3E%0A&");
-	    
+	     String url="http://m635674608.iteye.com/blog/2376153";
+		//send("vvvvvvvvvv", "%3Cp%3Eweqrqerqer%3C%2Fp%3E%0A&");
+		javaeye();
+		//System.out.println(getjavaeyeBlogContent(url));
+	          
+	         // fileDir=new File(fileDir, "1");
+			  //sendAll(fileDir);
+			//System.out.println(getTitle("1395728"));
+		
 	}
-	static  String ck="_user_behavior_=c8fb9227-9234-40a7-b054-e4ef95a29128; oscid=Qgdg8wRz2OcsdD4fp3FCcYwgebG1JwCmSPlVjKxReUbSNKmwdJT1KHu7q3xEwVlbKnTr%2FnXJ0phWKE%2Byr1pxcwGqY1FTfsDxg4t8O2Dr9el%2BbZSWQHtE6Sn7okJzEcTRkJ488fKQSm0ApT7CsKI%2BlwUb4rXZ3uJo1j4%2BJnWf3o46ZSBYvrRDow%3D%3D; aliyungf_tc=AQAAAMFLqBqcDw4AMzVi2hCFjQ+saSvA; Hm_lvt_a411c4d1664dd70048ee98afe7b28f0b=1512816806,1512817615,1512819582,1512826071; Hm_lpvt_a411c4d1664dd70048ee98afe7b28f0b=1512826487";
 	
 	
-	public static void send(String title,String content){
+	public static void sendAll(File file)throws Exception{
+		//File file = new File(fileDir, "1");
+		if(file.exists()){
+			for(File f:file.listFiles()){
+				String name=f.getName();
+				if(!name.contains("error")){
+					boolean flag = false;
+					for(int i=0;i<10;i++){
+						if((flag=sendOschina(name))){
+							System.out.println("文件名称："+name+" 上传结果："+flag+"上传成功>>>>>>>>>"); 
+							Thread.sleep(2000);
+							break;
+						}else{
+							System.out.println("文件名称："+name+" 上传结果："+flag+"上传失败>>>>>>>>>"+i); 
+							Thread.sleep(2000);
+						}
+					}
+					if(!flag){
+						FileUtils.write(new File("g:\\errorjavaeye.txt"), f.getAbsolutePath()+"\r\n", true);
+					}
+					System.out.println("文件名称："+name+" 上传结果："+flag); 
+					//return ;
+				}
+			}
+		}
+	}
+	
+	public static boolean sendOschina(String id)throws Exception{
+		 try{
+				String content = getCentent(id);
+				String title = getTitle(id);
+				//System.out.println(content);
+				//System.out.println(title);
+			    boolean flag =   send(title, content);
+			    //System.out.println(flag);
+			    return flag;
+				//return true;
+		 }catch(Exception e){
+			 e.printStackTrace();
+		 }
+		return false;
+		
+	}
+	
+	
+	public static void sendTest()throws Exception{
+		   String id = "1395728";
+			String content = getCentent(id);
+			String title = getTitle(id);
+			
+			System.out.println(content);
+			System.out.println(title);
+		
+		  boolean flag =   send(title, content);
+		   System.out.println(flag);
+	}
+	
+	public static String getCentent(String id)throws Exception{
+		File file = new File(fileDir,id);
+		Document document = Jsoup.parse(FileUtils.readFileToString(file));
+		String content = document.select("#blog_content").html();
+		String title = document.select(".blog_title").text();
+		content = title + content;
+		if(StringUtils.isNotBlank(content)){
+			content = URLEncoder.encode(content);
+			return content;
+		}
+		
+		return null;
+	}
+	
+	public static String getjavaeyeBlogContent(String url){
+		
+
+		 HttpGet httpGet = new HttpGet(url);
+		 httpGet.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+		// httpGet.setHeader("Accept-Encoding", "gzip, deflate");
+		 httpGet.setHeader("Accept-Language", "zh-CN,zh;q=0.9");
+	         
+		 
+        httpGet.setHeader("Host", "m635674608.iteye.com");
+        httpGet.setHeader("Upgrade-Insecure-Requests","1");
+        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36");
+        httpGet.setHeader("Cookie", "_javaeye_cookie_id_=1503710070572891; remember_me=yes; login_token=NzM4ODA2X2I0NDA4MGY1N2ExMWQyNmI3NWJmMDc3ZTFkMDI3NGM2%0A; Hm_lvt_e19a8b00cf63f716d774540875007664=1512878614,1512878771,1512879085,1512879708; Hm_lpvt_e19a8b00cf63f716d774540875007664=1512882009; dc_tos=p0qb89; dc_session_id=1512879708791_0.9657392941342531; _javaeye3_session_=BAh7CToQX2NzcmZfdG9rZW4iMUJDMnNGcE90MnBSQUZiRzUzUHdueld5SmkveCtHazFSOEJCYzlvSm9vOUE9Ogx1c2VyX2lkaQP2RQs6E3NpbXBsZV9jYXB0Y2hhIi03ZTIzZjYzM2I0Y2Y0NDVlYTY1ZWQ0ZWIzNGY5NjUwZjY5MjMwODcxOg9zZXNzaW9uX2lkIiVjM2Q3OGNhOWI4ZDkyYzkwZDJkYWRiMjc0ZjZkNDkyNg%3D%3D--07dd2b9d7a1a4557b3fd0a308a5b0b8b25f98084");
+     return		HttpClientUtil.sendGetRequest(httpGet, "utf-8",null);
+		
+		
+	}
+	
+	public static String getTitle(String id)throws Exception{
+		File file = new File(fileDir,id);
+		Document document = Jsoup.parse(FileUtils.readFileToString(file));
+		String content = document.select(".blog_title").select("a").text();
+		if(StringUtils.isNotBlank(content)){
+			content = URLEncoder.encode(content);
+			return content;
+		}
+		return ""+content;
+	}
+	
+	
+	
+	
+	static  String ck="_user_behavior_=c8fb9227-9234-40a7-b054-e4ef95a29128; __DAYU_PP=rvYMqBMra3b6iAaf3rfE3d13865607b8; oscid=Qgdg8wRz2OcsdD4fp3FCcYwgebG1JwCmSPlVjKxReUbSNKmwdJT1KHu7q3xEwVlbKnTr%2FnXJ0phWKE%2Byr1pxcwGqY1FTfsDxg4t8O2Dr9el%2BbZSWQHtE6Sn7okJzEcTRkJ488fKQSm0ApT7CsKI%2BlwUb4rXZ3uJo1j4%2BJnWf3o46ZSBYvrRDow%3D%3D; aliyungf_tc=AQAAAO99zFF2kg4A6/g/O9Km1c5JbjvV; Hm_lvt_a411c4d1664dd70048ee98afe7b28f0b=1512817615,1512819582,1512826071,1512869593; Hm_lpvt_a411c4d1664dd70048ee98afe7b28f0b=1512869593";
+	
+	
+	public static boolean send(String title,String content)throws Exception{
 		String draft = getdraft(title, content);
-		String sendData="draft="+draft+"&user_code=wu7fxTLkG9vDXBEn2b0Sq5lt8EyAjQlSvDay9EQd&id=&catalog=3427650&title=aaaaaaaaaaaaaccca&content_type=4&tags=&classification=428602&type=1&origin_url=&privacy=0&deny_comment=0&as_top=0&isRecommend=on&abstracts=&content=%3Cp%3EJava%E4%BB%A3%E7%A0%81%3C%2Fp%3E%0A%0A%3Cp%3E%3Cembed+allowscriptaccess%3D%22always%22+height%3D%2215%22+pluginspage%3D%22http%3A%2F%2Fwww.macromedia.com%2Fgo%2Fgetflashplayer%22+quality%3D%22high%22+src%3D%22http%3A%2F%2Fm635674608.iteye.com%2Fjavascripts%2Fsyntaxhighlighter%2Fclipboard_new.swf%22+type%3D%22application%2Fx-shockwave-flash%22+width%3D%2214%22+wmode%3D%22transparent%22%3E%3C%2Fembed%3E%3C%2Fp%3E%0A%0A%3Cp%3E%26nbsp%3B%3Ca+href%3D%22javascript%3Avoid()%22+onclick%3D%22code_favorites_do_favorite(this)%3Breturn+false%3B%22%3E%3Cimg+alt%3D%22%E6%94%B6%E8%97%8F%E4%BB%A3%E7%A0%81%22+src%3D%22http%3A%2F%2Fm635674608.iteye.com%2Fimages%2Ficon_star.png%22+%2F%3E%3C%2Fa%3E%3C%2Fp%3E%0A%0A%3Col%3E%0A%09%3Cli%3E%26lt%3Bpre%26nbsp%3Bclass%3D%26quot%3Bjava%26quot%3B%26nbsp%3Bname%3D%26quot%3Bcode%26quot%3B%26gt%3B%26nbsp%3B%26lt%3B%2Fpre%26gt%3B%26nbsp%3B%26nbsp%3B%3C%2Fli%3E%0A%3C%2Fol%3E%0A%0A%3Cp%3E%26nbsp%3B%26nbsp%3B%26nbsp%3B+%E9%80%9A%E8%BF%87SecurityContextHolder.getContext()%E8%8E%B7%E5%BE%97SecurityContext%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B+%E6%80%BB%E6%8E%A5%E5%8F%A3SecurityContextHolderStrategy%3C%2Fp%3E%0A%0A%3Cp%3E%3Cimg+alt%3D%22%22+src%3D%22http%3A%2F%2Fdl.iteye.com%2Fupload%2Fattachment%2F0065%2F7123%2Fb01eea45-c57a-335d-970c-2ef67ea65c58.png%22+%2F%3E%3C%2Fp%3E%0A%0A%3Cp%3Eprivate+static+void+initialize()+%7B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+if+((strategyName+%3D%3D+null)+%7C%7C+%26quot%3B%26quot%3B.equals(strategyName))+%7B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+%2F%2F+Set+default%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+strategyName+%3D+MODE_THREADLOCAL%3B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+%7D%3C%2Fp%3E%0A%0A%3Cp%3E%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+if+(strategyName.equals(MODE_THREADLOCAL))+%7B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+strategy+%3D+new+ThreadLocalSecurityContextHolderStrategy()%3B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+%7D+else+if+(strategyName.equals(MODE_INHERITABLETHREADLOCAL))+%7B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+strategy+%3D+new+InheritableThreadLocalSecurityContextHolderStrategy()%3B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+%7D+else+if+(strategyName.equals(MODE_GLOBAL))+%7B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+strategy+%3D+new+GlobalSecurityContextHolderStrategy()%3B%3Cbr+%2F%3E%0A%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B+%7D+else+%7B%3C%2Fp%3E%0A%0A%3Cp%3E%26nbsp%3B%3C%2Fp%3E%0A";
-		String reqURL="https://my.oschina.net/action/blog/save";
-		HttpPost httpPost   = new HttpPost(reqURL);
-		httpPost.setHeader("Cookie",ck);
-				httpPost.setHeader("Referer","https://my.oschina.net/xiaominmin/blog/edit");
-	    httpPost.setHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0");
-	    httpPost.setHeader("Host","my.oschina.net");
-		String str = HttpClientUtil.sendPostRequest(httpPost, sendData, false, "UTF-8","UTF-8");
-	    System.out.println(str);
+		if(StringUtils.isNotBlank(draft)){
+			Thread.sleep(1000);
+			String sendData="draft="+draft+"&user_code=wu7fxTLkG9vDXBEn2b0Sq5lt8EyAjQlSvDay9EQd&id=&catalog=3427650&title="+title+"&content_type=4&tags=&classification=428602&type=1&origin_url=&privacy=0&deny_comment=0&as_top=0&isRecommend=on&abstracts=&content="+content;
+			String reqURL="https://my.oschina.net/action/blog/save";
+			HttpPost httpPost   = new HttpPost(reqURL);
+			httpPost.setHeader("Cookie",ck);
+					httpPost.setHeader("Referer","https://my.oschina.net/xiaominmin/blog/edit");
+		    httpPost.setHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0");
+		    httpPost.setHeader("Host","my.oschina.net");
+			String str = HttpClientUtil.sendPostRequest(httpPost, sendData, false, "UTF-8","UTF-8");
+		    System.out.println(str);
+		    if(StringUtils.isNotBlank(str) && str.contains("id")){
+		    	return true;
+		    }
+		}
+		return false;
 	    
 	}
 	
@@ -82,51 +201,150 @@ public class OSchainBlog {
 	}
 	
 	
-	public static void tets()throws Exception {
+	public static void javaeye()throws Exception {
 		/*WebDriver driver = SeleniumUtil.initChromeDriverLocal();
 	    driver.get("https://my.oschina.net/xiaominmin/blog/edit");*/
 	    
-	    
-	    WebDriver firefoxDriver = SeleniumUtil.initFirefoxDriver();
-	    
-	    webGet(firefoxDriver, "http://m635674608.iteye.com/");
+	    //WebDriver firefoxDriver = SeleniumUtil.initChromeDriverLocal();
+	    //firefoxDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	    for(int i=30;i<=190;i++){
+	    	try{
+	    		for(int j=0;j<10;j++){
+	    			boolean flag =javaPage2(i);
+	    			if(flag){
+	    				break;
+	    			}
+	    		}
+	    	}catch(Exception e){
+	    		e.printStackTrace();
+	    	}
+	    }
+	}
+	
+	
+	   public static boolean javaPage2(int i){
+		   boolean flag = false;
+		   File file = new File(fileDir, ""+i);
+		   if(!file.exists()){
+			    file.mkdirs();
+		   }
+		   
+		   try{
+		    String urlList = "http://m635674608.iteye.com/?page="+i;
+		    System.out.println("当前抓取第"+i+"页数据>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		    //List<WebElement> elements = firefoxDriver.findElements(By.className("blog_main"));  
+		    String contentList = getjavaeyeBlogContent(urlList);
+		    if(StringUtils.isBlank(contentList)){
+		    	return flag;
+		    }
+		    
+		    List<String> pageURLList = new ArrayList<String>();
+			Document document = Jsoup.parse(contentList);
+			Elements elements = document.select(".blog_main");
+			System.out.println("当前页面有："+elements.size());
+			  for(Element e:elements){
+			    	String title = e.select(".blog_title").text();
+			       // System.out.println("titile==="+title);
+			        String href= e.select(".blog_title").select("a").get(0).attr("href");
+			        System.out.println("============"+baseURL+href);  
+			        pageURLList.add(baseURL+href);
+			  }
+			  for(String url:pageURLList){
+				  try {
+					  String id = url.replace(baseURL, "").replace("/blog/", "");
+					  File eyeFile = new File(file,""+id);
+					  if(eyeFile.exists()){
+						  continue;
+					  }
+					  System.out.println("url==="+url+" 开始下载");
+					  String content =getjavaeyeBlogContent(url);
+					  if(StringUtils.isNotBlank(content) && content.contains("m635674608")){
+						  FileUtils.write(eyeFile, content);
+						  Thread.sleep(2000);
+					  }else{
+						  System.out.println("文件加载不成功>>>>>>>>>>>>>>>"+content);
+						  FileUtils.write(new File(file,"error_"+id), content);
+						  Thread.sleep(2000);
+				
+					  }
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+			  }
+			  flag = true;
+		   }catch(Exception e){
+			   e.printStackTrace();
+		   }finally {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		   
+		   return flag;
+			  
+	   }
+	
+	
+   public static boolean javaPage( WebDriver firefoxDriver ,int i){
+	   boolean flag = false;
+	   File file = new File(fileDir, ""+i);
+	   if(!file.exists()){
+		    file.mkdirs();
+	   }
+	   
+	   try{
+	    webGet(firefoxDriver, "http://m635674608.iteye.com/?page="+i);
+	    System.out.println("当前抓取第"+i+"页数据>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 	    List<WebElement> elements = firefoxDriver.findElements(By.className("blog_main"));  
 	    System.out.println("elements:"+elements.size());
-	  /*  for(WebElement e:elements){
-	    	String title = e.findElement(By.className("blog_title")).getText();
-	        System.out.println("titile==="+title);
-	    }
-		*/
-	    
 	    
 	    List<String> pageURLList = new ArrayList<String>();
-	    
 		Document document = Jsoup.parse(firefoxDriver.getPageSource());
-		
 		  for(Element e:document.select(".blog_main")){
 		    	String title = e.select(".blog_title").text();
-		    	
 		        System.out.println("titile==="+title);
-		        
 		        String href= e.select(".blog_title").select("a").get(0).attr("href");
 		        System.out.println("============"+baseURL+href);  
-		        
 		        pageURLList.add(baseURL+href);
 		  }
-		  
-		  
 		  for(String url:pageURLList){
-			  webGet(firefoxDriver,url);
-			  document = Jsoup.parse(firefoxDriver.getPageSource());
-			  String id = url.replace(baseURL, "").replace("/blog/", "");
-			  //System.out.println("id==="+id);
-			  System.out.println("========"+document.text());
-			  
-			  
-			  FileUtils.write(new File(fileDir,""+id), document.toString());
+			  try {
+				  String id = url.replace(baseURL, "").replace("/blog/", "");
+				  File eyeFile = new File(file,""+id);
+				  if(eyeFile.exists()){
+					  continue;
+				  }
+				  webGet(firefoxDriver,url);
+				  String content = firefoxDriver.getPageSource();
+				  if(content.contains("zzm")){
+					  document = Jsoup.parse(content);
+					  System.out.println("id==="+id);
+					  //System.out.println("========"+document.text());
+					  FileUtils.write(eyeFile, document.toString());
+					  Thread.sleep(2000);
+				  }else{
+					  System.out.println("文件加载不成功>>>>>>>>>>>>>>>");
+				  }
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 		  }
-		  
+		  flag = true;
+	   }catch(Exception e){
+		   e.printStackTrace();
+	   }finally {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
+	   
+	   return flag;
+		  
+   }
 	
 	public static void webGet(WebDriver webDriver, String url) {
 		/*try{
@@ -136,18 +354,32 @@ public class OSchainBlog {
 			System.exit(0);
 		}*/
 		
-		webDriver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+	
 		try {
 			webDriver.get(url);
 		} catch (Exception e) {
 			
-			  try { Thread.sleep(100); } catch (InterruptedException e1) {
-			  e1.printStackTrace(); } JavascriptExecutor js =
+			  try { Thread.sleep(100); 
+			  Thread.sleep(1000);
+			 JavascriptExecutor js =
 			  (JavascriptExecutor) webDriver;
-			  js.executeScript("window.stop();");
+			 // js.executeScript("window.stop();");
 			 System.out.println("已停止加载页面》》》》》》》》》》》》》》》》》》》》》》》》");
-			 
-		}
+			 if(url.contains("?")){
+				  webDriver.get(url+"&temp="+System.currentTimeMillis());
+			 }else{
+				  webDriver.get(url+"?temp="+System.currentTimeMillis());
+			 }
+			
+		} catch (Exception e1) {
+			  e1.printStackTrace(); 
+			  webDriver.get(url+"&temp="+System.currentTimeMillis());
+			  //webDriver.close();
+			  //webDriver = SeleniumUtil.initChromeDriverLocal();
+			 // webDriver.navigate().refresh();
+			  
+			  }
 	}
-
+}
+	
 }
