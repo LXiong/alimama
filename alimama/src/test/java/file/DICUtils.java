de18df3a5f8b80cdad35d40681aedc56bd9ded7c
lovego <lovego@DESKTOP-29Q2ORF>
2018-07-06 09:27:18 +0800

    test

--- alimama/src/test/java/file/DICUtils.java
+++ alimama/src/test/java/file/DICUtils.java
@@ -2,11 +2,13 @@ package file;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.Sets;
 
 public class DICUtils {
 	@SuppressWarnings("deprecation")
@@ -14,13 +16,13 @@ public class DICUtils {
 		File file = new File("E:\\eclipse-workspace\\alimama\\alimama\\src\\main\\resources\\main.dic");
 		File fileOut = new File("E:\\main.dic");
 		List<String> readLines = FileUtils.readLines(file);
-		List<String> readLinesOut =new ArrayList<>();
+		Set<String> readLinesOut =Sets.newHashSet();
 		for(String s :readLines) {
 			if(StringUtils.isNotBlank(s)) {
 				if(s.length() >= 2) {
 					/*s = s.substring(0, 2);
 					FileUtils.write(fileOut, s, true);*/
-					readLinesOut.add( s.substring(0, 2));
+					readLinesOut.add( s.substring(0, 1));
 				}
 		}
 	}
